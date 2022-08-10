4f46393ebe40b11f9302579d44b0cd3fef55cead
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2022-08-10 14:01:16 +0300

    configs etc.

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 function startGame() {
     // starting a new game
-    var messagesElement = document.getElementById('messages');
+
+    let playerName: string = 'Audrey';
+    logPLayer(playerName);
+
+    let messagesElement = document.getElementById('messages');
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
   }
+
+function logPLayer(name) {
+  console.log(`New game starting for player: ${name}`);
+}
   
-  document.getElementById('startGame')!.addEventListener('click', startGame);
+document.getElementById('startGame')!.addEventListener('click', startGame);
